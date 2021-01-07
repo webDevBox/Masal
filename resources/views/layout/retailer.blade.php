31c10347641c32c5e1a84326ba8ebf9810ba0845
Khubaib Waheed <33741348+khubaibwaheed@users.noreply.github.com>
2021-01-07 13:16:21 +0500

    a

--- resources/views/layout/retailer.blade.php
+++ resources/views/layout/retailer.blade.php
@@ -113,98 +113,39 @@
 
 
 <li class="nav-item dropdown dropdown-notification mr-25"><a class="nav-link" href="javascript:void(0);" data-toggle="dropdown"><i class="fa fa-bell" aria-hidden="true"></i>
-    @if($remain > 0)<span class="badge badge-pill badge-danger badge-up"> {{ $remain }} </span> @endif </a>
+@if($remain > 0)<span class="badge badge-pill badge-danger badge-up"> {{ $remain }} </span> @endif </a>
+
 <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
 <li class="dropdown-menu-header">
 <div class="dropdown-header d-flex">
 <h4 class="notification-title mb-0 mr-auto">Notifications</h4>
-<div class="badge badge-pill badge-light-primary">6 New</div>
+<div class="badge badge-pill badge-light-primary">{{ $remain }} New Messages</div>
 </div>
 </li>
+@if($remain > 0)
+<li class="scrollable-container media-list">
+    <a class="d-flex" href="{{ route('chat') }}">
+    <div class="media d-flex align-items-start">
+    <div class="media-left">
+    <div class=""><img src="{{ asset('app-assets/images/portrait/small/chat.png')}}" alt="avatar" width="32" height="32"></div>
+    </div>
+    <div class="media-body">
+    <p class="media-heading"><span class="font-weight-bolder">Chat Modals</span>
+    </div>
+    </div>
+    </a>
+    </li>
+@endif
 </ul>
+
+
 </li>
 
 </ul>
 </div>
 </nav>
-<ul class="main-search-list-defaultlist d-none">
-<li class="d-flex align-items-center"><a href="javascript:void(0);">
-<h6 class="section-label mt-75 mb-0">Files</h6>
-</a></li>
-<li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100" href="app-file-manager.html">
-<div class="d-flex">
-<div class="mr-75"><img src="{{ asset('app-assets/images/icons/xls.png')}}" alt="png" height="32"></div>
-<div class="search-data">
-<p class="search-data-title mb-0">Two new item submitted</p><small class="text-muted">Marketing Manager</small>
-</div>
-</div><small class="search-data-size mr-50 text-muted">&apos;17kb</small>
-</a></li>
-<li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100" href="app-file-manager.html">
-<div class="d-flex">
-<div class="mr-75"><img src="{{ asset('app-assets/images/icons/jpg.png')}}" alt="png" height="32"></div>
-<div class="search-data">
-<p class="search-data-title mb-0">52 JPG file Generated</p><small class="text-muted">FontEnd Developer</small>
-</div>
-</div><small class="search-data-size mr-50 text-muted">&apos;11kb</small>
-</a></li>
-<li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100" href="app-file-manager.html">
-<div class="d-flex">
-<div class="mr-75"><img src="{{ asset('app-assets/images/icons/pdf.png')}}" alt="png" height="32"></div>
-<div class="search-data">
-<p class="search-data-title mb-0">25 PDF File Uploaded</p><small class="text-muted">Digital Marketing Manager</small>
-</div>
-</div><small class="search-data-size mr-50 text-muted">&apos;150kb</small>
-</a></li>
-<li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100" href="app-file-manager.html">
-<div class="d-flex">
-<div class="mr-75"><img src="{{ asset('app-assets/images/icons/doc.png')}}" alt="png" height="32"></div>
-<div class="search-data">
-<p class="search-data-title mb-0">Anna_Strong.doc</p><small class="text-muted">Web Designer</small>
-</div>
-</div><small class="search-data-size mr-50 text-muted">&apos;256kb</small>
-</a></li>
-<li class="d-flex align-items-center"><a href="javascript:void(0);">
-<h6 class="section-label mt-75 mb-0">Members</h6>
-</a></li>
-<li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100" href="app-user-view.html">
-<div class="d-flex align-items-center">
-<div class="avatar mr-75"><img src="{{ asset('app-assets/images/portrait/small/avatar-s-8.jpg')}}" alt="png" height="32"></div>
-<div class="search-data">
-<p class="search-data-title mb-0">John Doe</p><small class="text-muted">UI designer</small>
-</div>
-</div>
-</a></li>
-<li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100" href="app-user-view.html">
-<div class="d-flex align-items-center">
-<div class="avatar mr-75"><img src="{{ asset('app-assets/images/portrait/small/avatar-s-1.jpg')}}" alt="png" height="32"></div>
-<div class="search-data">
-<p class="search-data-title mb-0">Michal Clark</p><small class="text-muted">FontEnd Developer</small>
-</div>
-</div>
-</a></li>
-<li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100" href="app-user-view.html">
-<div class="d-flex align-items-center">
-<div class="avatar mr-75"><img src="{{ asset('app-assets/images/portrait/small/avatar-s-14.jpg')}}" alt="png" height="32"></div>
-<div class="search-data">
-<p class="search-data-title mb-0">Milena Gibson</p><small class="text-muted">Digital Marketing Manager</small>
-</div>
-</div>
-</a></li>
-<li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100" href="app-user-view.html">
-<div class="d-flex align-items-center">
-<div class="avatar mr-75"><img src="{{ asset('app-assets/images/portrait/small/avatar-s-6.jpg')}}" alt="png" height="32"></div>
-<div class="search-data">
-<p class="search-data-title mb-0">Anna Strong</p><small class="text-muted">Web Designer</small>
-</div>
-</div>
-</a></li>
-</ul>
-<ul class="main-search-list-defaultlist-other-list d-none">
-<li class="auto-suggestion justify-content-between"><a class="d-flex align-items-center justify-content-between w-100 py-50">
-<div class="d-flex justify-content-start"><span class="mr-75" data-feather="alert-circle"></span><span>No results found.</span></div>
-</a></li>
-</ul>
-<!-- END: Header-->
+
+
 
 
 <!-- BEGIN: Main Menu-->
@@ -235,14 +176,6 @@
 </li>
 <li class=" nav-item"><a class="d-flex align-items-center" href="{{ route('chat') }}"><i class="fa fa-comments" style="color: #8c4fec;" aria-hidden="true"></i><span class="menu-title text-truncate" data-i18n="Calendar">Chat</span></a>
 </li>
-
-
-
-
-
-
-
-
 </ul>
 </div>
 </div>
