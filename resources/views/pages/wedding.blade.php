4cd38dd1b1a09b9f4fcc6e0283c73ca890794beb
Khubaib Waheed <33741348+khubaibwaheed@users.noreply.github.com>
2021-02-10 11:35:03 +0500

    a

--- resources/views/pages/wedding.blade.php
+++ resources/views/pages/wedding.blade.php
@@ -26,23 +26,95 @@
     </div>
 </section>
 
-<div class="row">
-    @foreach ($retailer_real as $row)
+<section class="section-gallerylooks gallerylooks-theme mt-internal mt-internal-sm-alt">
+    <div class="container container-lg">
+    <div class="row">
+    <div class="gallerylooks-images">
+    <div class="grid">
+    
+        @foreach ($retailer_real as $row)
+        @php
+            $name=\App\User::find($row->retailer);
+            $data=\App\wedding::find($row->wedding);
+        @endphp
+    <div class="gallerylook-image grid-item" id="slidesGrid0">
+    <div class="grid-item-content">
+
+        @if($row->type == 'image')
+        <img src="{{ asset('images/'.$row->file) }}" />
+        @endif
+        @if($row->type == 'video')
+        <video style="width: 600px; height:500px;" src="{{ asset('images/'.$row->file) }}" type="video/mp4" controls>
+        @endif
+        @if($row->type == 'link')
+        <iframe style="width: 600px; height:500px;" src="{{ $row->file }}" frameborder="2" allowfullscreen>
+        </iframe>
+        @endif
+
+       
+    <div class="grid-item-info">
    
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" style="text-align:center;">
-            @if($row->type == 'image')
-            <img alt="" style="width: 200px; height: 300px; padding:15px;" src="{{ asset('images/'.$row->file) }}">
-            @endif
-            @if($row->type == 'video')
-            <video style="width: 200px; height: 300px; padding:15px;" src="{{ asset('images/'.$row->file) }}" type="video/mp4" controls>
-            @endif
-            @if($row->type == 'link')
-            <iframe style="width: 200px; height: 300px; padding:15px;" src="{{ $row->file }}" frameborder="0" allowfullscreen>
-            </iframe>
-            @endif
-        </div>
+    <a href="{{route('wedding_detail', array('id' => $row->id))}}" data-property="description">
+    </a>
+
+    <div class="social-networks social-share social-share-multi">
+        <ul>
+            <li>
+                <span class="share-title">
+                    Share:
+                </span>
+            </li>
+                        <li>
+                            <a href="https://www.pinterest.com/sophiatolli/"
+                               target="_blank"
+                               data-property="pinterest-share-multi"
+                               data-social-title="Y11895A | Zena"
+                               data-social-url="{{ asset('images/'.$row->file) }}"
+                               data-social-picture="{{ asset('images/'.$row->file) }}"
+                               data-social-description=""
+                               title="Pinterest"
+                               aria-label="Share on Pinterest">
+                                <i class="fa fa-pinterest-p"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/sophiatolli"
+                               target="_blank"
+                               data-property="twitter-share-multi"
+                               data-social-title="Y11895A | Zena"
+                               data-social-url="real-brides.html#slidesGrid0"
+                               data-social-picture="{{ asset('images/'.$row->file) }}"
+                               data-social-description=""
+                               title="Twitter"
+                               aria-label="Share on Twitter">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                        </li>
+        </ul>
+    </div>
+    
+ 
+    
+    @if($row->type == 'image')
+    <div data-property="description" class="header">
+    <h3>Wedding</h3>
+    <h2>{{$data->name}}</h2>
+    </div>
+    @endif
+    
+    </div>
+    </div>
+    </div>
     @endforeach
-</div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </section>
+
+
+
+
 
 @endsection
 
